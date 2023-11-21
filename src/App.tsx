a48231e32ac177a21a73737f46e190e4dc45e25d
Abolfazl Akbarzadeh <abolfazlakbarzadeh80@gmail.com>
2023-11-21 20:56:45 +0330

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import "@/assets/styles/styles.scss"
+import { Routes, Route } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <Routes>
+
+    </Routes>
+  )
+}
+
+export default App

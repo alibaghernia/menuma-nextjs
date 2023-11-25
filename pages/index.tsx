fe518bfa8ed07c73c57edccfb3341838b302fb22
Abolfazl Akbarzadeh <abolfazlakbarzadeh80@gmail.com>
2023-11-25 20:30:39 +0330

    - enamad title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import CoffeShopProvider from "@/providers/coffee_shop/provider"
 import { ProviderContext } from "@/providers/main/provider"
+import Head from "next/head"
 import { useContext, useEffect } from "react"
 
 function Home() {
@@ -11,16 +12,22 @@ function Home() {
   }, [setLoading])
 
   return (
+    <>
+    <Head>
+      <title>870745</title>
+    </Head>
     <div className="min-h-screen">
       <div className="absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] flex flex-col gap-3 items-center">
         <div className="text-[5rem] text-gray-400">
-          870745
+          منوما
         </div>
         <div className="text-[1.5rem] text-gray-600">
           در حال توسعه هستیم
         </div>
       </div>
     </div>
+
+    </>
   )
 }
 

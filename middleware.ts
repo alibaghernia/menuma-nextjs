524262cd2587b6480180a8a5f842176259b60712
Abolfazl Akbarzadeh <abolfazlakbarzadeh80@gmail.com>
2023-11-27 01:49:49 +0330

    - debug middleware

--- middleware.ts
+++ middleware.ts
@@ -5,12 +5,12 @@ export function middleware(request: NextRequest) {
   const appDomain = process.env.NEXT_PUBLIC_MENUMA_DOMAIN;
   if (!appDomain) {
     console.log("check app domain");
-    process.exit(1)
+    process.exit(1);
   }
   const url = new URL(request.url);
-  const host = request.headers.get('host');
-  const protocol = request.headers.get('x-forwarded-proto') || url.protocol;
-  const reqUrl = request.url.replace(`${url.host}`, host || '')
+  const host = request.headers.get("host");
+  const protocol = `${request.headers.get("x-forwarded-proto")}:` || url.protocol;
+  const reqUrl = request.url.replace(`${url.host}`, host || "");
   if (
     !reqUrl.startsWith(`http://${appDomain}/`) &&
     !reqUrl.startsWith(`https://${appDomain}/`) &&
@@ -21,8 +21,12 @@ export function middleware(request: NextRequest) {
   ) {
     const domain_name = host?.split(".") || [];
 
-    return NextResponse.rewrite(new URL(
-        `${protocol}://${host}/${(domain_name.length > 1 ? domain_name[domain_name.length - 2] : domain_name.toString())}`
-    ));
+    return NextResponse.rewrite(
+      `${protocol}//${host}/${
+        domain_name.length > 1
+          ? domain_name[domain_name.length - 2]
+          : domain_name.toString()
+      }`
+    );
   }
 }

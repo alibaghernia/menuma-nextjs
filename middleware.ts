480b6cae9c15411388f1e7af61610909672d0fd6
Abolfazl Akbarzadeh <abolfazlakbarzadeh80@gmail.com>
2023-11-27 01:38:07 +0330

    - debug middleware

--- middleware.ts
+++ middleware.ts
@@ -9,6 +9,7 @@ export function middleware(request: NextRequest) {
   }
   const url = new URL(request.url);
   const host = request.headers.get('host');
+  const protocol = request.headers.get('x-forwarded-proto') || url.protocol;
   const reqUrl = request.url.replace(`${url.host}`, host || '')
   if (
     !reqUrl.startsWith(`http://${appDomain}/`) &&
@@ -21,7 +22,7 @@ export function middleware(request: NextRequest) {
     const domain_name = host?.split(".") || [];
 
     return NextResponse.rewrite(new URL(
-        `${url.protocol}//${host}/${(domain_name.length > 1 ? domain_name[domain_name.length - 2] : domain_name.toString())}`, request.url
+        `${protocol}//${host}/${(domain_name.length > 1 ? domain_name[domain_name.length - 2] : domain_name.toString())}`
     ));
   }
 }

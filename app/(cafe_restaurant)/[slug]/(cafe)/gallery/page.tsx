627283513efbc22ec15943812c767edb4ebdc0fc
Abolfazl Akbarzadeh <abolfazlakbarzadeh80@gmail.com>
2024-02-29 02:41:04 +0330

    remove panorama image

--- app/(cafe_restaurant)/[slug]/(cafe)/gallery/page.tsx
+++ app/(cafe_restaurant)/[slug]/(cafe)/gallery/page.tsx
@@ -4,16 +4,12 @@ import { FlexItem } from '@/components/common/flex_item/flex_item';
 import { Section } from '@/components/common/section/section';
 import { Footer } from '@/components/core/footer/footer';
 import Navbar from '@/components/core/navbar/navbar';
-import { CoffeeShopPageProvider } from '@/providers/coffee_shop/page_provider';
 import { CoffeeShopProviderContext } from '@/providers/coffee_shop/provider';
 import { useCurrentBreakpoints } from '@/utils/hooks';
-import { withCafeeShopProfile } from '@/utils/serverSideUtils';
-import { GetServerSideProps } from 'next';
 import React, { useContext, useMemo } from 'react';
 import Masonry from 'react-responsive-masonry';
 import dynamic from 'next/dynamic';
 import { GalleryImage } from '@/components/pages/gallery/image/types';
-import image360 from '@/assets/images/360-image-2.jpg';
 
 const GalleryImage = dynamic(
   () => import('@/components/pages/gallery/image/image'),
@@ -36,46 +32,7 @@ function GalleryPage() {
   }, [breakpoints.breakpoints]);
 
   const images = useMemo(() => {
-    const images: GalleryImage[] = [
-      {
-        src: 'https://i.pinimg.com/564x/6b/61/9b/6b619b18f89bcdceec7e223c28e99f8e.jpg',
-        title: 'Image 1',
-        childs: [
-          {
-            title: 'image 2',
-            src: 'https://i.pinimg.com/564x/8e/66/92/8e6692a623146d86c16b811cdfa05853.jpg',
-            information: 'this is a test information',
-          },
-          {
-            title: 'image 3',
-            src: 'https://i.pinimg.com/564x/80/e9/27/80e9279d123d9b280146456603b1ebcd.jpg',
-          },
-          {
-            title: 'image 4',
-            src: image360.src,
-            is_panorama: true,
-          },
-          {
-            title: 'image 5',
-            src: 'https://i.pinimg.com/736x/90/c4/89/90c4895984a1eb57eaecafc18173f5cc.jpg',
-          },
-          {
-            title: 'image 5',
-            src: 'https://i.pinimg.com/736x/90/c4/89/90c4895984a1eb57eaecafc18173f5cc.jpg',
-          },
-          {
-            title: 'image 5',
-            src: 'https://i.pinimg.com/736x/90/c4/89/90c4895984a1eb57eaecafc18173f5cc.jpg',
-          },
-        ],
-      },
-
-      {
-        title: 'image 4',
-        src: image360.src,
-        is_panorama: true,
-      },
-    ];
+    const images: GalleryImage[] = [];
     return images.map((image, idx) => (
       <div key={idx} className="relative rounded-[.862rem] overflow-hidden">
         <GalleryImage {...image} zoomable />
